537c9ccbe3605fcb343c3bf70a7dfdc5200a1222
SofiaFer889 <sofiaelizabetfernandez@gmail.com>
2023-05-15 10:45:21 -0300

    primer commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+alert('Bienvenido')
+
+function cantidadDeProduco (){
+    let producto = parseFloat(prompt('indique cuantos productos desea comprar'))
+    return producto
+}
+
+const precio = 150
+
+function calcularMonto (producto, precio){
+    if(producto&&precio){
+        const montoTotal = precio*producto
+        return montoTotal
+    }
+}
+
+const producto = cantidadDeProduco()
+const totalAPagar = calcularMonto(producto, precio)
+
+alert('Su total a pagar es de:'+ '\n' + totalAPagar)
+alert('Gracias por su compra!!')
+ 
+console.log(calcularMonto)
